fd36a8458a0994711a95b4a902538ea4f283c2e2
LaToya Hinton, PhD <94628744+Gatheroxign34@users.noreply.github.com>
2024-06-02 16:57:03 -0400

    Update BB_Department_Average_Salary.sql
    New Filtered Queries

--- BB_Department_Average_Salary.sql
+++ BB_Department_Average_Salary.sql
@@ -1,8 +1,27 @@
 SELECT "Job_Family" AS "Department", AVG("Salary") FROM employees GROUP BY "Job_Family";
 
+/**This query was run just to understand which departments exist in BananaByte with their range of salaries**/ 
+
 SELECT 
 	"Employee_ID", 
 	"Job_Family", 
 	"Salary", AVG("Salary") OVER(PARTITION BY "Job_Family") AS "Dept_avg"
 FROM 
 	employees;  
+/**This query was run to look at individual employees' salaries as they relate to department average salaries**/
+
+SELECT "Job_Role" AS "Role", AVG("Salary") FROM employees WHERE "Job_Family" IN ('Software Development')
+GROUP BY "Job_Role";
+
+/**This query was run to look instead at software engineer salaries within the specific department of Software Development**/ 
+
+SELECT 
+	"Employee_ID", 
+	"Job_Family",
+	"Job_Role", 
+	"Salary", AVG("Salary") 
+	 OVER(PARTITION BY "Job_Family") AS "Dept_avg"
+FROM 
+	employees;  
+
+/** This query was run to look at each individual employee's salary within the department of Software Development**/
